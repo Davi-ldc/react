6ff95b05ccb4e24f5c6e75fdd2067c3a90eda541
Davi-ldc <96698943+Davi-ldc@users.noreply.github.com>
2023-08-30 16:03:01 -0300

    hitbox

--- Projects/car-physics/src/Track.jsx
+++ Projects/car-physics/src/Track.jsx
@@ -8,9 +8,8 @@ import { useControls  } from 'leva'
 
 
 export function Track() {
-  const { colisoes, debug } = useControls({
-    colisoes: false,
-    debug: false,
+  const { HitBox } = useControls({
+    HitBox: false,
   });
 
   const result = useLoader(
@@ -37,24 +36,24 @@ export function Track() {
           map={colorMap}
         />
       </mesh>
-      { colisoes &&
-      <><ColliderBox position={[1.75, 0, 0.5]} scale={[0.3/3.4, 1/3.4, 0.3/3.4]} debug={debug}/>
-      <ColliderBox position={[2.5, 0, -1.4]} scale={[0.3/3.4, 1/3.4, 0.3/3.4]} debug={debug}/>
-      <ColliderBox position={[0.6, 0, -3.8]} scale={[0.3/3.4, 1/3.4, 0.3/3.4]} debug={debug}/>
-      <ColliderBox position={[-1.95, 0, -5.18]} scale={[0.3/3.4, 1/3.4, 0.3/3.4]} debug={debug}/>
-      <ColliderBox position={[-5.55, 0, -3.05]} scale={[0.3/3.4, 1/3.4, 0.3/3.4]} debug={debug}/>
-      <ColliderBox position={[-4.4, 0, -1.77]} scale={[0.3/3.4, 1/3.4, 0.3/3.4]} debug={debug}/>
-      <ColliderBox position={[-7.03, 0, -0.76]} scale={[0.3/3.4, 1/3.4, 0.3/3.4]} debug={debug}/>
-      <ColliderBox position={[-4.75, 0, 2.73]} scale={[0.3/3.4, 1/3.4, 0.3/3.4]} debug={debug}/>
-      <ColliderBox position={[-3.05, 0, 3.4]} scale={[0.3/3.4, 1/3.4, 0.3/3.4]} debug={debug}/>
-      <ColliderBox position={[-0.83, 0, 3.2]} scale={[0.3/3.4, 1/3.4, 0.3/3.4]} debug={debug}/>
+
+      <><ColliderBox position={[1.75, 0, 0.5]} scale={[0.3/3.4, 1/3.4, 0.3/3.4]} debug={HitBox}/>
+      <ColliderBox position={[2.5, 0, -1.4]} scale={[0.3/3.4, 1/3.4, 0.3/3.4]} debug={HitBox}/>
+      <ColliderBox position={[0.6, 0, -3.8]} scale={[0.3/3.4, 1/3.4, 0.3/3.4]} debug={HitBox}/>
+      <ColliderBox position={[-1.95, 0, -5.18]} scale={[0.3/3.4, 1/3.4, 0.3/3.4]} debug={HitBox}/>
+      <ColliderBox position={[-5.55, 0, -3.05]} scale={[0.3/3.4, 1/3.4, 0.3/3.4]} debug={HitBox}/>
+      <ColliderBox position={[-4.4, 0, -1.77]} scale={[0.3/3.4, 1/3.4, 0.3/3.4]} debug={HitBox}/>
+      <ColliderBox position={[-7.03, 0, -0.76]} scale={[0.3/3.4, 1/3.4, 0.3/3.4]} debug={HitBox}/>
+      <ColliderBox position={[-4.75, 0, 2.73]} scale={[0.3/3.4, 1/3.4, 0.3/3.4]} debug={HitBox}/>
+      <ColliderBox position={[-3.05, 0, 3.4]} scale={[0.3/3.4, 1/3.4, 0.3/3.4]} debug={HitBox}/>
+      <ColliderBox position={[-0.83, 0, 3.2]} scale={[0.3/3.4, 1/3.4, 0.3/3.4]} debug={HitBox}/>
       
-      <ColliderBox position={[-1.85,0,0.385]} scale={[0.05, 1, 0.13]} debug={debug}/>
-      <ColliderBox position={[-1.85,0,-0.385]} scale={[0.05, 1, 0.13]} debug={debug}/>
-      <ColliderBox position={[-2.28,0,0.385]} scale={[0.05, 1, 0.13]} debug={debug}/>
-      <ColliderBox position={[-2.28,0,-0.385]} scale={[0.05, 1, 0.13]} debug={debug}/>
-      <ColliderBox position={[-4.39,0,1.125]} scale={[0.13, 1, 0.13]} debug={debug}/>
-      <ColliderBox position={[-4.39,0,1.9]} scale={[0.13, 1, 0.13]} debug={debug}/>
+      <ColliderBox position={[-1.85,0,0.385]} scale={[0.05, 1, 0.13]} debug={HitBox}/>
+      <ColliderBox position={[-1.85,0,-0.385]} scale={[0.05, 1, 0.13]} debug={HitBox}/>
+      <ColliderBox position={[-2.28,0,0.385]} scale={[0.05, 1, 0.13]} debug={HitBox}/>
+      <ColliderBox position={[-2.28,0,-0.385]} scale={[0.05, 1, 0.13]} debug={HitBox}/>
+      <ColliderBox position={[-4.39,0,1.125]} scale={[0.13, 1, 0.13]} debug={HitBox}/>
+      <ColliderBox position={[-4.39,0,1.9]} scale={[0.13, 1, 0.13]} debug={HitBox}/>
       
       <ColliderBox position={[-2.86,0,-0.9]} scale={[0.35, 1, 0.35]}/>
       <ColliderBox position={[-3.33,0,-0.9]} scale={[0.35, 1, 0.35]}/>
@@ -68,7 +67,7 @@ export function Track() {
       <ColliderBox position={[-4.53,0,-0.65]} scale={[0.1, 0.5, 0.1]}/>
       <ColliderBox position={[-4.15,0,-0.67]} scale={[0.1, 0.5, 0.1]}/>
       <ColliderBox position={[-4.9,0,-0.58]} scale={[0.1, 0.5, 0.1]}/>
-      <ColliderBox position={[-0.3,0,1]} scale={[0.1, 0.5, 0.1]}/></>}
+      <ColliderBox position={[-0.3,0,1]} scale={[0.1, 0.5, 0.1]}/></>
 
       <Ramp />
 
