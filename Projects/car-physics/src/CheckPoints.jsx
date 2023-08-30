80ff80a5e3cd19a705ab6b4b48f68099679d4ea7
Davi-ldc <96698943+Davi-ldc@users.noreply.github.com>
2023-08-30 09:26:36 -0300

    Update CheckPoints.jsx

--- Projects/car-physics/src/CheckPoints.jsx
+++ Projects/car-physics/src/CheckPoints.jsx
@@ -40,17 +40,17 @@ export function CheckPoints(startTime){
     }
 
     const playCheckpointSound = () => {
-        checkpointSound.volume = 0.05;
+        //checkpointSound.volume = 0.05;
         checkpointSound.play();
       };
     
     const playLapSound = () => {
-        lapSound.volume = 0.4;
+        //lapSound.volume = 0.4;
         lapSound.play();
       };
     
     const playFinishSound = () => {
-        finishSound.volume = 0.05;
+        //finishSound.volume = 0.05;
         finishSound.play();
       };
       
